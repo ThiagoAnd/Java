be874d726df6d115ace4f8b3d4208ffcef242f84
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-03-08 08:51:04 -0300

    Feat: Atualização de projeto

---
+++ EE/MasterMicroservicesWorkspace/limits-services/src/main/java/com/in28minutesmicroservices/limitsservices/LimitsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutesmicroservices.limitsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServicesApplication.class, args);
+	}
+
+}
