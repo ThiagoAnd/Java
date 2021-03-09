7c2d1d83863d6efa057766346d536a973a3c7e11
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-03-09 16:50:31 -0300

    Feat: Atualização de projeto

--- EE/MasterMicroservicesWorkspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/feignclients/CurrencyExchangeProxy.java
+++ EE/MasterMicroservicesWorkspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/feignclients/CurrencyExchangeProxy.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.in28minutes.microservices.currencyconversionservice.bean.CurrencyConversion;
 
 // Nome da aplicação que vc vai ligar e url
-@FeignClient(name="currency-exchange",url="localhost:8000")
+//@FeignClient(name="currency-exchange",url="localhost:8000")
+
+//Só fazendo isso abaixo o eureka ja busca o nome e retorna o endereço
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
