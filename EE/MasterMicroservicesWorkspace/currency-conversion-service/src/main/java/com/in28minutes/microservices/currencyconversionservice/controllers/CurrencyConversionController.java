76f0f271c3d3e7ffb067847f3b7cde1d9d37d0ed
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-03-09 15:53:27 -0300

    Feat: Atualização de projeto

--- EE/MasterMicroservicesWorkspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ EE/MasterMicroservicesWorkspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -35,6 +35,8 @@ public class CurrencyConversionController {
 		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" "+"Rest Template ");
 	}
 	
+	
+	
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversionFeign (@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity ) {
 		
