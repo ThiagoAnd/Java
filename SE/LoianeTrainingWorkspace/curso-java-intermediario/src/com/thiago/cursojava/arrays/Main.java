513134ab279b35a4c3229707ff6d894a1cca80c9
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-03-13 18:51:13 -0300

    Feat: Atualização de projeto

--- SE/LoianeTrainingWorkspace/curso-java-intermediario/src/com/thiago/cursojava/arrays/Main.java
+++ SE/LoianeTrainingWorkspace/curso-java-intermediario/src/com/thiago/cursojava/arrays/Main.java
@@ -7,6 +7,30 @@ public class Main {
 		String[] teste = new String[2];
 		
 		teste[1] = "asdf";
+		teste[0]= "wwww";
+		
+		for(String x : teste) {
+			System.out.println(x);
+			
+		}
+		
+		char[][] quadrado = new char[3][2];
+		
+		for(int i=0;i<quadrado.length;i++) {
+			System.out.println("Valor i: "+i);
+			
+			for(int j = 0;j<quadrado[i].length;j++) {
+				System.out.println(j);
+			}
+			System.out.println("\n");
+		}
+		
+		String[] p = new String[5];
+		System.out.println(p[2]);
+		
+		
+		
+		
 		
 	}
 }
