6c540471d2c534e050271c8c6f18f09945c9af87
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-03-12 23:27:40 -0300

    Feat: Atualização de projeto

---
+++ SE/LoianeTrainingWorkspace/curso-java-intermediario/src/com/thiago/cursojava/scanner/Leitura.java
@@ -0,0 +1,15 @@
+package com.thiago.cursojava.scanner;
+
+import java.util.Scanner;
+
+public class Leitura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Testando o scanner");
+		scan.nextInt();
+	
+	}
+
+}
